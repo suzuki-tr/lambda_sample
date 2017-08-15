634235aeee1bbc76422840d8ec615c66d71b7289
suzuki-tr <toru_zk@yahoo.co.jp>
2017-08-15 22:09:18 +0900

    reply image

--- LINEbot/lambda_function.py
+++ LINEbot/lambda_function.py
@@ -1,40 +1,103 @@
 #https://pypi.python.org/pypi/line-bot-sdk/1.3.0
 #pip install line-bot-sdk -t .
-#compress all files to tar.gz
-#upload to lambda
+#pip install pillow -t . #PIL
 
+# import library
 from linebot import LineBotApi
-from linebot.models import TextSendMessage
+from linebot.models import TextSendMessage,ImageSendMessage
 from linebot.exceptions import LineBotApiError
 from linebot.models.responses import MessageContent
+import boto3
 import json
+import io
+import PIL.Image as Image
 
-reply_url = "https://api.line.me/v2/bot/message/reply"
-push_url = "https://api.line.me/v2/bot/message/push"
+# static params
 channelaccesstoken="dummy"
-userid = "dummy"
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
 
-def reply_msg(replytoken,text):
+##################
+# Methods with line-bot-sdk
+def reply_txtmsg(replytoken,text):
     line_bot_api = LineBotApi(channelaccesstoken)
     try:
         line_bot_api.reply_message(replytoken, TextSendMessage(text))
     except LineBotApiError as e:
         print(e)
 
-def push_msg(userid,text):
+def reply_imgmsg(replytoken,imgurl):
+    line_bot_api = LineBotApi(channelaccesstoken)
+    try:
+        line_bot_api.reply_message(replytoken, ImageSendMessage(imgurl,imgurl))
+    except LineBotApiError as e:
+        print(e)
+
+def push_txtmsg(userid,text):
     line_bot_api = LineBotApi(channelaccesstoken)
     try:
         line_bot_api.push_message(userid, TextSendMessage(text))
     except LineBotApiError as e:
         print(e)
 
-def get_image(imageid):
+def push_imgmsg(userid,imgurl):
     line_bot_api = LineBotApi(channelaccesstoken)
-    return line_bot_api.get_message_content(imageid)
+    try:
+        line_bot_api.push_message(userid, ImageSendMessage(imgurl,imgurl))
+    except LineBotApiError as e:
+        print(e)
 
+def get_image_content(imageid):
+    line_bot_api = LineBotApi(channelaccesstoken)
+    message_content = line_bot_api.get_message_content(imageid)
+    return message_content.content
+
+def save_image(imageid,path):
+    line_bot_api = LineBotApi(channelaccesstoken)
+    message_content = line_bot_api.get_message_content(imageid)
+    print ('message_content:{}'.format(type(message_content)))
+    with open(path, mode='wb') as fd:
+        for chunk in message_content.iter_content():
+            fd.write(chunk)
+
+
+##################
+# Methods with AWS
+def save_fileobj_s3(data,bucket,key):
+    targetbucket = s3_resource.Bucket(bucket)
+    targetobj = targetbucket.Object(key)
+    targetobj.upload_fileobj(data)
+
+def save_file_s3(datafile,bucket,key):
+    targetbucket = s3_resource.Bucket(bucket)
+    targetobj = targetbucket.Object(key)
+    targetobj.upload_fileobj(datafile)
+
+# get presigned url of s3 object
+def get_presigned_url(bucket,key):
+    url = s3_client.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket,
+            'Key': key
+        }
+    )
+    return url
+
+##################
+# Methods with PIL
+def resize_image(image_path, resized_path):
+    print ('{}\n{}'.format(image_path, resized_path))
+    with Image.open(image_path) as image:
+        width = 320 #static
+        height = 320*image.size[1]/image.size[0]
+        image.thumbnail((width,height))
+        image.save(resized_path, format='JPEG')
 
 def lambda_handler(event, context):
-    # event is json type
+    print('start of lambda_handler')
+
+    # 'event' is json object
     print('EVENT DUMPS :' + json.dumps(event, sort_keys=True, indent=4))
     # event["body"] is string
     # json.loads(event["body"]) is dictional type
@@ -44,14 +107,38 @@ def lambda_handler(event, context):
 
     # process of each event type
     messagetype = json.loads(event["body"])["events"][0]["message"]["type"]
+
     if messagetype == 'text':
         receivedmessage = json.loads(event["body"])["events"][0]["message"]["text"]
         print('Received Message:' + receivedmessage)
-        reply_msg(replytoken,receivedmessage)
+        # reply message copy of received
+        reply_txtmsg(replytoken,receivedmessage)
 
     elif messagetype == 'image':
-        imageid = json.loads(event["body"])["events"][0]["message"]["id"]
-        img = get_image(imageid)
-        img.content
-        reply_msg(replytoken,"imageid:" + str(imageid))
+        print('Received Message:' + str(json.loads(event["body"])["events"][0]["message"]))
 
+        #1). get image id
+        imageid = json.loads(event["body"])["events"][0]["message"]["id"]
+
+        #2). get image content
+        data = get_image_content(imageid) #binary data <type 'str'>
+        datastream = io.BytesIO(data)
+
+        #3). create thumnail
+        outputbuf = io.BytesIO()
+        resize_image(datastream, outputbuf)
+
+        #4). save to s3
+        bucket = 'uswestbucket-suzuki'
+        key = 's3test/' + imageid + '.jpg'
+        outputbuf.seek(0)
+        save_file_s3(outputbuf,bucket,key)
+
+        #5). get Rekognition result
+
+        #6). reply line message
+        imgurl = get_presigned_url(bucket,key)
+        print (imgurl)
+        reply_imgmsg(replytoken, imgurl)
+
+    print('end of lambda_handler')
